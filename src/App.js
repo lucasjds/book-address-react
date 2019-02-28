3c954c14c6367ed05b5f525830ee45a97c1cba5e
lucasjds <lucassouza690@gmail.com>
2019-02-28 16:23:18 -0500

    bootstrap adde , edit method created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-import Registration from "./Registration";
-//import TodoItems from "./TodoItems";
+import bootstrap from "bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   inputElement = React.createRef();
   constructor() {
     super();
     this.state = {
-      contacts: []
+      contacts: [],
+      form: {
+        id: 0,
+        first_name: "",
+        last_name: "",
+        phone: "",
+        email: ""
+      },
+      create: true
     };
   }
   deleteMember(member) {
@@ -15,7 +23,7 @@ class App extends Component {
       id: member.id
     };
     console.log(data);
-    fetch("https://untitled-2pnbnmu167mj.runkit.sh/delete", {
+    fetch("hhttps://test4-vv6e1xcajcg8.runkit.sh/delete", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
@@ -36,20 +44,46 @@ class App extends Component {
       });
   }
 
-  editMember(member) {
-    var data = {
-      id: member.id,
-      first_name: member.first_name,
-      last_name: member.last_name,
-      phone: member.phone,
-      email: member.email
-    };
-    console.log(data);
+  handleEdit(member) {
+    this.setState({
+      form: {
+        id: member.id,
+        first_name: member.first_name,
+        last_name: member.last_name,
+        phone: member.phone,
+        email: member.email
+      },
+      create: false
+    });
   }
+
+  editMember = e => {
+    e.preventDefault();
+    fetch("https://test4-vv6e1xcajcg8.runkit.sh/edit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.state.form)
+    })
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        if (data == "success") {
+          this.setState({ msg: "Thanks for registering" });
+        }
+      })
+      .catch(function(err) {
+        console.log("error", err);
+      });
+  };
 
   componentDidMount() {
     let self = this;
-    fetch("https://untitled-2pnbnmu167mj.runkit.sh/contacts/all", {
+    fetch("https://test4-vv6e1xcajcg8.runkit.sh/contacts/all", {
       method: "GET"
     })
       .then(function(response) {
@@ -67,12 +101,78 @@ class App extends Component {
       });
   }
 
+  addItem = e => {
+    e.preventDefault();
+    fetch("https://test4-vv6e1xcajcg8.runkit.sh/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.state.form)
+    })
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        if (data == "success") {
+          this.setState({ msg: "Thanks for registering" });
+        }
+      })
+      .catch(function(err) {
+        console.log("error", err);
+      });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      form: { ...this.state.form, [name]: value }
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <Registration inputElement={this.inputElement} />
+        <div className="todoListMain">
+          <div className="header">
+            <form onSubmit={this.state.create ? this.addItem : this.editMember}>
+              <input type="hidden" name="id" value={this.state.form.id} />
+              <input
+                placeholder="First Name"
+                onChange={this.handleChange}
+                name="first_name"
+                className="form-control"
+                value={this.state.form.first_name}
+              />
+              <input
+                placeholder="Last Name"
+                onChange={this.handleChange}
+                name="last_name"
+                className="form-control"
+                value={this.state.form.last_name}
+              />
+              <input
+                placeholder="Phone"
+                onChange={this.handleChange}
+                name="phone"
+                className="form-control"
+                value={this.state.form.phone}
+              />
+              <input
+                placeholder="Email"
+                onChange={this.handleChange}
+                name="email"
+                className="form-control"
+                value={this.state.form.email}
+              />
+              <button type="submit"> Submit </button>
+            </form>
+          </div>
+        </div>
         <div className="panel panel-default p50 uth-panel">
-          <table className="table table-hover">
+          <table className="table table-striped table-dark">
             <thead>
               <tr>
                 <th>First Name</th>
@@ -90,7 +190,7 @@ class App extends Component {
                   <td>{member.email}</td>
                   <td>{member.phone}</td>
                   <td>
-                    <a onClick={() => this.editMember(member)}>Edit</a>
+                    <a onClick={() => this.handleEdit(member)}>Edit</a>
                     <a onClick={() => this.deleteMember(member)}>Delete</a>
                   </td>
                 </tr>
